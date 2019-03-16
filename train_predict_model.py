53cdbd3e282e2b64f4f01b9d03b5be256901a2df
punkproger <punkproger@gmail.com>
2019-03-17 01:54:01 +0200

    Increased accuracy, and got f1 macro score as 0.837 by RandomtreeSearch

--- train_predict_model.py
+++ train_predict_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import sklearn
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
@@ -10,6 +11,8 @@ from keras.optimizers import Adam
 from keras import callbacks
 
 from keras.layers.normalization import BatchNormalization
+
+from sklearn.metrics import f1_score
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -26,21 +29,17 @@ input_size     = 0
 output_size    = 3
 
 def load_data_from_csv(csv_path):
-    global input_size
     db = pd.read_csv(csv_path)
-
+    db = db.drop(columns=['objid', 'ra', 'dec','rowc','colc', 'rowv', 'colv'])
     col_size = db.shape[1]
 
-    if(input_size == 0):
-        input_size = col_size - 2
-
-    X = db.iloc[:, 1:(col_size - 1)]
-    Y_raw = db.iloc[:, (col_size - 1):col_size]
+    X = db.drop(columns=['class'])
+    Y_raw = db['class']
     Y = []
-    for index, row in Y_raw.iterrows():
-        if  (row[0] == 0):
+    for val in Y_raw:
+        if  (val == 0):
             Y.append(np.array([1,0,0]))
-        elif(row[0] == 1):
+        elif(val == 1):
             Y.append(np.array([0,1,0]))
         else:
             Y.append(np.array([0,0,1]))
@@ -51,6 +50,23 @@ def load_data_from_csv(csv_path):
     Y = np.asarray(Y)
     return X, Y
 
+def getIdxOfMax(arr):
+    idx=0
+    maxIdx = 0
+    max=-1
+    for i in arr:
+        if(max < i):
+            max = i
+            maxIdx = idx
+        idx += 1
+    return maxIdx
+
+
+def toOrigFormat(Y_val):
+    Y_res = []
+    for i in Y_val:
+        Y_res.append(getIdxOfMax(i))
+    return Y_res
 
 def create_model(): 
     model = Sequential()
@@ -61,15 +77,6 @@ def create_model():
 
     model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=learning_rate), metrics=["accuracy"])
     return model
-
-
-
-history = {
-    'train-loss': [], 'train-accuracy': [],
-    'test-loss': [], 'test-accuracy': [],
-}
-
-
 
 X_train, Y_train = load_data_from_csv('data/train.csv')
 X_test, Y_test   = load_data_from_csv('data/val.csv')
@@ -86,3 +93,8 @@ history = model.fit(X_train, Y_train,
 score = model.evaluate(X_test, Y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+Y_result = toOrigFormat(model.predict(X_test))
+Y_test = toOrigFormat(Y_test)
+
+print("Macro-averaged f1-score: ", f1_score(Y_test, Y_result, average='macro'))
